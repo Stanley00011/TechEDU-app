b03c6314629805e82516a8ebaa129013ad938754
Olajide Stanley Ajao <73467145+Stanley00011@users.noreply.github.com>
2023-08-03 22:41:24 +0400

    oo

--- subscribe.php
+++ subscribe.php
@@ -1,7 +1,7 @@
 <?php
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Get the user's phone number and email from the form
+    
     $phone = $_POST["phone"];
     $email = $_POST["email"];
       
@@ -11,3 +11,60 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     echo "Error: Invalid request.";
 }
 ?>
+
+
+
+<?php
+        // Display job listings
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo '<div class="job-listing">';
+                echo '<h2>' . $row['title'] . '</h2>';
+                echo '<p>Company: ' . $row['company'] . '</p>';
+                echo '<p>Location: ' . $row['location'] . '</p>';
+                
+                echo '</div>';
+            }
+        } else {
+            echo 'No job listings available.';
+        }
+
+        
+        $conn->close();
+        ?>
+
+
+<?php
+           // Replace 'YOUR_SLACK_API_KEY' with your actual Slack API key
+$slackApiKey = 'YOUR_SLACK_API_KEY';
+
+
+function sendMessageToSlack($message) {
+    $slackUrl = "https://slack.com/api/chat.postMessage";
+    $channel = "general"; 
+    $data = array(
+        "text" => $message,
+        "channel" => $channel
+    );
+
+    $headers = array(
+        "Authorization: Bearer " . $slackApiKey;
+        "Content-Type: application/json"
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $slackUrl);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+}
+
+            ?>
+
+
